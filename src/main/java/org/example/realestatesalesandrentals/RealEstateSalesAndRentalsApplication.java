3e608fbabd6fb0d970e947f769359c6905297189
HakobAvagyan <hakobavagyan14@gmail.com>
2026-02-25 23:56:48 +0400

    test

---
+++ src/main/java/org/example/realestatesalesandrentals/RealEstateSalesAndRentalsApplication.java
@@ -0,0 +1,13 @@
+package org.example.realestatesalesandrentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateSalesAndRentalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateSalesAndRentalsApplication.class, args);
+    }
+
+}
