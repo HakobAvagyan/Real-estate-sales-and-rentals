aa3bc77d07e0d02d9e3c0922ce809b00a6e1a796
poghosyanrob <robert.poghosyan.g@gmail.com>
2026-04-13 16:16:23 +0400

    Refactor UserUpdateDto; remove commented-out fields and streamline data structure for user updates

--- persistence/src/main/java/org/example/dto/user/UserUpdateDto.java
+++ persistence/src/main/java/org/example/dto/user/UserUpdateDto.java
@@ -11,17 +11,12 @@ import java.time.LocalDate;
 @Data
 public class UserUpdateDto {
 
-//    private int id;
     private String name;
     private String surname;
     private String email;
     private String phone;
     private LocalDate birthDate;
     private String passportDetails;
-//    @NotBlank(message = "New password must not be blank")
-//    @Size(min = 8, max = 20, message = "New password must be between 8 and 20 characters")
-//    private String password;
     private String picName;
-//    private Role role;
 
 }
